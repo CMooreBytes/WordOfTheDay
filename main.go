ba8cd7b6a432e414eaf4a2b493dfbed48b3cde93
Devon <devongarrett@Asgard-MacBook.local>
2019-04-26 15:50:22 -0400

    .

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 
 var svc WordServiceInterface
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	result := new(Result)
+	result.Word = "word"
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	svc := new (WordService)
 	result := svc.GetWord()
@@ -17,6 +24,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main(){
 	log.Println("Starting up")
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/api/word", handler)
+	http.HandleFunc("/test/word", testHandler)
 	log.Fatal(http.ListenAndServe(":8001", nil))
 }
