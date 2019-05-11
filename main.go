cf00c1a9d6e8ce1432316ca96005da276bed4ed7
Devon <devongarrett@Asgard-MacBook.wowway.com>
2019-05-10 20:08:14 -0400

    refactored

--- main.go
+++ main.go
@@ -8,10 +8,16 @@ import (
 )
 
 func main(){
-	log.Println("Initializing the router...")
+	log.Println("Initializing the service...")
 	handler := http.NewServeMux()
 	log.Println("Registering API endpoints...")
-	router.Register(handler)
+	handler.HandleFunc("/api/word/wotd", router.GetWordHandler)
+	handler.HandleFunc("/api/word/scramble", router.ScrambleHandler)
+	handler.HandleFunc("/api/word/puzzle",router.PuzzleHandler)
+	handler.HandleFunc("/test", router.TestHandler)
+	log.Println("Registering static site file handler...")
+	handler.HandleFunc("/", router.DefaultHandler)
+	log.Println("Getting HTTP server port number...")
 	port_number := router.PortNumber()
 	log.Println(fmt.Sprintf("Starting server. Listening on port %s", port_number))
 	log.Fatal(http.ListenAndServe(port_number, handler))
--- pkg/router/router.go
+++ pkg/router/router.go
@@ -4,6 +4,11 @@ import (
 	"net/http"
 	"fmt"
 	"os"
+	"html/template"
+	"log"
+	"path"
+	"encoding/json"
+	"github.com/cmoorebytes/wordoftheday/pkg/wordservice"
 )
 
 const default_port_number = 8000
@@ -18,10 +23,54 @@ func PortNumber() string{
 	
 }
 
-func Register(handler *http.ServeMux){
-	handler.HandleFunc("/api/word/wotd", GetWordHandler)
-	handler.HandleFunc("/api/word/scramble", ScrambleHandler)
-	handler.HandleFunc("/api/word/puzzle", PuzzleHandler)
-	handler.HandleFunc("/test", TestHandler)
-	handler.HandleFunc("/", DefaultHandler)
+func DefaultHandler(w http.ResponseWriter, r *http.Request) {
+	base := path.Base(r.URL.Path)
+	if(base == "/") {
+		base = "index.html"
+	}
+	switch path := path.Ext(r.URL.Path); path {
+		case ".js":
+			http.ServeFile(w, r, "wwwroot/js/" + base)
+		case ".css":
+			http.ServeFile(w, r, "wwwroot/css/" + base)
+		default:
+			t,err := template.New(base).ParseFiles("wwwroot/" + base)
+			if(err != nil){
+				log.Fatal(err)
+			}
+			
+			result := new(Result)
+			result.Word, result.ScrambledWord = wordservice.GetScrambledWord()
+			t.Execute(w, result)
+	}
+}
+
+func GetWordHandler (w http.ResponseWriter, r *http.Request) {
+	result := new(Result)
+	result.Word, result.ScrambledWord = wordservice.GetScrambledWord()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+}
+
+func PuzzleHandler(w http.ResponseWriter, r *http.Request) {
+	_, scrambled_word := wordservice.GetScrambledWord()
+	result := PuzzleResult { ScrambledWord: scrambled_word }
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+}
+
+func ScrambleHandler(w http.ResponseWriter, r *http.Request) {
+	query_string := r.URL.Query()
+	word := query_string.Get("word")
+	result := Result {Word: word, ScrambledWord: wordservice.Scramble(word) }
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+}
+
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	word := "word"
+	result := Result { Word: word, ScrambledWord: wordservice.Scramble(word) }
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
 }
